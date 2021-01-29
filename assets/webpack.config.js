446524d8dcc28feb2dcc7f04224ea359952bf0d9
Daniel P. Shannon <daniel@fleet.i.ng>
2021-01-29 05:11:15 -0500

    🐛 Fix Webpack config.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -2,6 +2,7 @@ const autoprefixer = require('autoprefixer')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const cssnano = require('cssnano')
 const fiber = require('fibers')
+const fs = require('fs')
 const mime = require('mime')
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
@@ -14,110 +15,108 @@ const getLinkedPath = (...fragments) =>
         )
     )
 
-module.exports = async () => {
-    const IS_PRODUCTION = !!process.env.NODE_ENV && process.env.NODE_ENV === 'production'
+const IS_PRODUCTION = !!process.env.NODE_ENV && process.env.NODE_ENV === 'production'
 
-    return {
-        mode: process.env.NODE_ENV || 'development',
-        devtool: 'source-map',
-        context: getLinkedPath('src'),
-        resolve: {
-            enforceExtension: false,
-            modules: [
-                getLinkedPath('./node_modules'),
-                getLinkedPath('./src'),
-                getLinkedPath('./vendor'),
-            ],
+module.exports = {
+    mode: process.env.NODE_ENV || 'development',
+    devtool: 'source-map',
+    context: getLinkedPath('src'),
+    resolve: {
+        enforceExtension: false,
+        modules: [
+            getLinkedPath('./node_modules'),
+            getLinkedPath('./src'),
+            getLinkedPath('./vendor'),
+        ],
+    },
+    entry: {
+        bundle: './index',
+        Hyphenopoly: 'hyphenopoly/Hyphenopoly',
+    },
+    output: {
+        path: getLinkedPath('public'),
+        publicPath: 'https://hot.garb.ag/',
+        filename: '[name].js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [
+                    { loader: MiniCSSExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    { loader: 'postcss-loader' },
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCSSExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                    },
+                    {
+                        loader: 'resolve-url-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass'),
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(otf|woff2?)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext][query]',
+                },
+            },
+            {
+                test: /\.wasm$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'bin/[name][ext][query]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        IS_PRODUCTION && new CleanWebpackPlugin(),
+        new MiniCSSExtractPlugin({
+            filename: `styles/[name].css`
+        }),
+    ].filter(Boolean),
+    cache: {
+        buildDependencies: {
+            config: [__filename],
         },
-        entry: {
-            bundle: './index',
-            Hyphenopoly: 'hyphenopoly/Hyphenopoly',
+        type: 'filesystem',
+    },
+    snapshot: {
+        buildDependencies: {
+            timestamp: true,
         },
-        output: {
-            path: getLinkedPath('public'),
-            publicPath: 'https://hot.garb.ag/',
-            filename: '[name].js',
-        },
+        managedPaths: [
+            getLinkedPath('./node_modules'),
+        ],
         module: {
-            rules: [
-                {
-                    test: /\.css$/i,
-                    use: [
-                        { loader: MiniCSSExtractPlugin.loader },
-                        { loader: 'css-loader' },
-                        { loader: 'postcss-loader' },
-                    ]
-                },
-                {
-                    test: /\.s[ac]ss$/i,
-                    use: [
-                        {
-                            loader: MiniCSSExtractPlugin.loader,
-                        },
-                        {
-                            loader: 'css-loader',
-                        },
-                        {
-                            loader: 'postcss-loader',
-                        },
-                        {
-                            loader: 'resolve-url-loader',
-                        },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                implementation: require('sass'),
-                            },
-                        },
-                    ],
-                },
-                {
-                    test: /\.(otf|woff2?)$/i,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'fonts/[name][ext][query]',
-                    },
-                },
-                {
-                    test: /\.wasm$/i,
-                    type: 'asset/resource',
-                    generator: {
-                        filename: 'bin/[name][ext][query]',
-                    },
-                },
-            ],
+            timestamp: true,
         },
-        plugins: [
-            IS_PRODUCTION && new CleanWebpackPlugin(),
-            new MiniCSSExtractPlugin({
-                filename: `styles/[name].css`
-            }),
-        ].filter(Boolean),
-        cache: {
-            buildDependencies: {
-                config: [__filename],
-            },
-            type: 'filesystem',
+        resolve: {
+            timestamp: true,
         },
-        snapshot: {
-            buildDependencies: {
-                timestamp: true,
-            },
-            managedPaths: [
-                getLinkedPath('./node_modules'),
-            ],
-            module: {
-                timestamp: true,
-            },
-            resolve: {
-                timestamp: true,
-            },
-            resolveBuildDependencies: {
-                timestamp: true,
-            },
+        resolveBuildDependencies: {
+            timestamp: true,
         },
-        experiments: {
-            asset: true,
-        },
-    }
+    },
+    experiments: {
+        asset: true,
+    },
 }
