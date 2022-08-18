6aade885c3a0404215e68ad1f0fdd07df56b8a5d
Daniel P. Shannon <daniel@fleet.i.ng>
2022-08-18 18:30:52 -0400

    Fix local Pandoc URL.

--- assets/webpack.config.mjs
+++ assets/webpack.config.mjs
@@ -16,7 +16,7 @@ const __dirname = path.dirname(__filename)
 const IS_PRODUCTION = !!process.env.NODE_ENV && process.env.NODE_ENV === 'production'
 const PUBLIC_PATH = IS_PRODUCTION
     ? 'https://hot.garb.ag/'
-    : 'https://pandoc.here/'
+    : 'https://pandoc.localhost/'
 
 const fonts = globbySync('./src/vendor/fonts/**/*.scss')
     .map(fn => fn.replace('src/vendor', 'vendor'))
